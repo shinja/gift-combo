e2ae67ce1b9a6ef665b53d736d366bb4d5420cdd
finley <FinleyLu@trevi.cc>
2021-05-30 14:31:45 +0800

    Add rxjs Add gift bubble/recyle/sent.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,111 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="relative">
+    <div :key="sent">sent: {{ sent }}</div>
+    <div>stack : {{ Math.max(0, count - sent) }}</div>
+    <button ref="button">click Me!</button>
+    <div class="animation absolute left-0 bottom-0">
+      <div>{{ count }}</div>
+      <div class="bar" :style="{ height: `${count * 3}px` }"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import { fromEvent, interval, merge } from "rxjs";
+import {
+  switchMap,
+  takeUntil,
+  takeWhile,
+  tap,
+  finalize,
+  mapTo,
+} from "rxjs/operators";
+
+const config = {
+  UPDATE_TIME: 100,
+  COMBO_TIME: 3 * 1000,
+  SEND_TIME: 0.5 * 1000,
+};
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  data() {
+    return {
+      sent: 0,
+      count: 0,
+      MAX_COUNT: config.COMBO_TIME / config.UPDATE_TIME,
+    };
+  },
+
+  mounted() {
+    const press = fromEvent(this.$refs.button, "pointerdown");
+    const up = fromEvent(this.$refs.button, "pointerup");
+
+    const send = press.pipe(
+      switchMap(() =>
+        interval(config.SEND_TIME)
+          .pipe(takeUntil(recycle))
+          .pipe(tap(() => this.send()))
+      )
+    );
+
+    const bubble = press.pipe(
+      switchMap(() =>
+        interval(config.UPDATE_TIME)
+          .pipe(
+            takeWhile(() => this.count < this.MAX_COUNT),
+            takeUntil(recycle),
+            mapTo(1)
+          )
+          .pipe(tap((v) => (this.count += v)))
+      )
+    );
+
+    const recycle = up.pipe(
+      switchMap(() =>
+        interval(config.UPDATE_TIME)
+          .pipe(
+            takeWhile(() => this.count > 0),
+            takeUntil(bubble),
+            mapTo(-1)
+          )
+          .pipe(
+            tap((v) => {
+              this.count += v;
+            }),
+            finalize(() => {
+              this.sent = this.count;
+              if (this.count === 0) {
+                this.reset();
+              }
+            })
+          )
+      )
+    );
+
+    merge(bubble, recycle, send).subscribe();
+  },
+
+  methods: {
+    send() {
+      this.sent = Math.max(0, this.count - this.sent);
+    },
+    reset() {
+      this.sent = 0;
+      this.count = 0;
+      this.level = 0;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.bar {
+  width: 60px;
+  height: 120px;
+  background: linear-gradient(#e66465, #9198e5);
 }
 </style>
